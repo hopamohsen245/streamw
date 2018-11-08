55e52f452c12f31c256df56c52f68eb04bb13e9b
hopamohsen245 <44228723+hopamohsen245@users.noreply.github.com>
2018-11-08 14:35:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`We Dont Talk ANY MORE `,"http://twitch.tv/plexy")
+client.user.setGame(`sell BOT CREDIT `,"http://twitch.tv/plexy")
 client.user.setStatus("dnd")
 });
 
